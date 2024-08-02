f6a3314d5c337d215bc7dc5ea52d4a871447e27b
MariaLuiza12344 <128408641+MariaLuiza12344@users.noreply.github.com>
2024-08-02 17:56:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,132 @@
+const numerosenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numerosenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onlick = aumentaTamanho;
+
+function diminui(){
+    if (tamanhoSenha >1){ 
+    tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
